96ed97f1b2bb26a4ba2ca589d30e011beba52141
MattBanda <matthewbanda2322@gmail.com>
2022-03-13 17:41:52 -0500

    Fixed bug with calculation of total amount

--- fuel-quote-module.js
+++ fuel-quote-module.js
@@ -20,15 +20,15 @@ var objUser = [
         zipcode: "77054"
     }
 ]
-
-const gallons = document.getElementsByName('gallons')[0].value
+ 
 const clientAddress = `${objUser[0].primary_address} ${objUser[0].secondary_address} ${objUser[0].city} ${objUser[0].state} ${objUser[0].zipcode}`
 const marketPrice = 5.00
-const totalDue = gallons * marketPrice
-
-document.getElementById('address').innerHTML = clientAddress
 document.getElementById('price').innerHTML = marketPrice
+ 
+document.getElementById('address').innerHTML = clientAddress
 
 function calculate() {
+    gallons = document.getElementsByName('gallons')[0].value
+    totalDue = gallons * marketPrice
     document.getElementById('total').innerHTML = totalDue
 }
